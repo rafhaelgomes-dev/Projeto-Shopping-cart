a709180743b8725ed0b4ce2390e71093f237ed7f
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-07-12 07:18:02 -0300

    Requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,19 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Testa se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts()).toEqual('function');
+  });
+
+  test('Execute a função fetchProducts com o argumento computador e teste se fetch foi chamada', async () => {
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  test('este se, ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+ 
 });
