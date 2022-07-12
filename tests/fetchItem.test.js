38c7cd9bdf0cbc47c630e15b15ae72b3c7efd7e8
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-07-12 09:55:04 -0300

    Requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,13 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Testa se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toEqual('function');
+  });
+
+  test('Teste se, ao chamar a função fetchItem com o argumento do item MLB1615760527, a função fetch utiliza o endpoint', async () => {
+    await fetchItem('MLB1615760527')
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  
 });
